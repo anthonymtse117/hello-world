ec5551ed44d25f005340f4fae6dce3220fccb8a4
anthonymtse117 <anthonymtse117@gmail.com>
2016-09-13 15:53:05 -0400

    Update HelloWorld.java

--- Notes/src/HelloWorld.java
+++ Notes/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	
 	public static void main(String[]args){
 	System.out.println("Hello World, My name is Anthony!");
+	System.out.println("Java is pretty cool :)");
 	
 	}
 }
